b82f8f53b72d5bade76a3d369ece5856e92aecd0
Matt <matthewd102@hotmail.com>
2021-05-27 21:33:16 +0100

    Documented existing codebase
    This will enable class, function and variable summaries in Visual Studio IntelliSense, should make understanding current codebase easier for newcomers.

--- Functions/Hash.cs
+++ Functions/Hash.cs
@@ -4,8 +4,17 @@ using System.Text.RegularExpressions;
 
 namespace Functions
 {
+  /// <summary>
+  /// Hash utility functions to create and validate hashes
+  /// </summary>
   public static class Hash
   {
+    /// <summary>
+    /// Create a SHA-1 hash
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="source">Source encoding. Defaults to UTF-8. Pass in any other value for Unicode</param>
+    /// <returns>The input string as a SHA-1 hash</returns>
     public static string CreateSHA1Hash(string input, string source = "UTF8")
     {
       if (input == null)
@@ -13,21 +22,25 @@ namespace Functions
         return null;
       }
 
-      using (var sha1 = new SHA1Managed())
+      using var sha1 = new SHA1Managed();
+      
+      var bytes = source == "UTF8" ? Encoding.UTF8.GetBytes(input) : Encoding.Unicode.GetBytes(input);
+      var hash = sha1.ComputeHash(bytes);
+      var sb = new StringBuilder(hash.Length * 2);
+
+      foreach (var b in hash)
       {
-        var bytes = source == "UTF8" ? Encoding.UTF8.GetBytes(input) : Encoding.Unicode.GetBytes(input);
-        var hash = sha1.ComputeHash(bytes);
-        var sb = new StringBuilder(hash.Length * 2);
-
-        foreach (var b in hash)
-        {
-          sb.Append(b.ToString("X2"));
-        }
-
-        return sb.ToString().ToUpper();
+        sb.Append(b.ToString("X2"));
       }
+
+      return sb.ToString().ToUpper();
     }
 
+    /// <summary>
+    /// Check that the string is a valid SHA-1 hash with regex
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     public static bool IsStringSHA1Hash(string input)
     {
       if (string.IsNullOrWhiteSpace(input))
--- Functions/PwnedResponse.cs
+++ Functions/PwnedResponse.cs
@@ -7,8 +7,20 @@ using System.Text;
 
 namespace Functions
 {
+  /// <summary>
+  /// Helper class which creates the response for a pwned passwords API request
+  /// </summary>
   public static class PwnedResponse
   {
+    /// <summary>
+    /// Creates a response from the 
+    /// </summary>
+    /// <param name="req">Client HTTP request message</param>
+    /// <param name="code">Status code to return to the client</param>
+    /// <param name="content">The content to pass to the client. Either use this to return a status message or stream</param>
+    /// <param name="stream">Stream from a file to pass as a response. This is primarily used for serving hash prefix files to clients.</param>
+    /// <param name="lastModified">Last Modified date time to set for the response headers</param>
+    /// <returns>Constructed HttpResponseMessage to serve to the client</returns>
     public static HttpResponseMessage CreateResponse(HttpRequestMessage req, HttpStatusCode code, string content = null, Stream stream = null, DateTimeOffset? lastModified = null)
     {
       var msg = new HttpResponseMessage(code);
