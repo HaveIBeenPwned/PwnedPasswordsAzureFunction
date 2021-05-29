d46547e2d328c7de2d870129ca28ea5f2fa7f60c
Matt <matthewd102@hotmail.com>
2021-05-29 19:42:30 +0100

    Improved tolerance of invalid JSON input

--- Functions/Range.cs
+++ Functions/Range.cs
@@ -45,63 +45,71 @@ namespace Functions
                 return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Content-Type must be application/json");
             }
 
-            // Get JSON POST request body
-            PwnedPasswordAppend data = await req.Content.ReadAsAsync<PwnedPasswordAppend>();
+            try
+            {
+                // Get JSON POST request body
+                PwnedPasswordAppend data = await req.Content.ReadAsAsync<PwnedPasswordAppend>();
 
-            if (data == null)
-            {
-                // Json wasn't parsed from POST body, bad request
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing JSON body");
-            }
+                if (data == null)
+                {
+                    // Json wasn't parsed from POST body, bad request
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing JSON body");
+                }
 
-            if (string.IsNullOrEmpty(data.SHA1Hash))
-            {
-                // Empty SHA-1 hash, bad request
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing SHA-1 hash");
-            }
-            if (!Hash.IsStringSHA1Hash(data.SHA1Hash))
-            {
-                // Invalid SHA-1 hash, bad request
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "The SHA-1 hash was not in a valid format");
-            }
+                if (string.IsNullOrEmpty(data.SHA1Hash))
+                {
+                    // Empty SHA-1 hash, bad request
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing SHA-1 hash");
+                }
+                if (!Hash.IsStringSHA1Hash(data.SHA1Hash))
+                {
+                    // Invalid SHA-1 hash, bad request
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "The SHA-1 hash was not in a valid format");
+                }
 
-            if (string.IsNullOrEmpty(data.NTLMHash))
-            {
-                // Empty NTLM hash, bad request
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing NTLM hash");
-            }
-            if (!Hash.IsStringNTLMHash(data.NTLMHash))
-            {
-                // Invalid NTLM hash, bad request
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "The NTLM hash was not in a valid format");
-            }
+                if (string.IsNullOrEmpty(data.NTLMHash))
+                {
+                    // Empty NTLM hash, bad request
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing NTLM hash");
+                }
+                if (!Hash.IsStringNTLMHash(data.NTLMHash))
+                {
+                    // Invalid NTLM hash, bad request
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "The NTLM hash was not in a valid format");
+                }
 
-            if (data.Prevalence <= 0)
+                if (data.Prevalence <= 0)
+                {
+                    // Prevalence not set or invalid value, bad request
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing or invalid prevalence value");
+                }
+
+                log.Info("Received valid Pwned Passwords append request");
+
+                BlobStorage storage = new BlobStorage(log);
+                bool? newEntry = storage.UpdateHash(data.SHA1Hash, data.Prevalence);
+                if (newEntry == null)
+                {
+                    // Returned null, that means that the item was unable to be added, internal server error
+                    return PwnedResponse.CreateResponse(req, HttpStatusCode.InternalServerError, "Unable to add entry to Pwned Passwords");
+                }
+
+                if (newEntry.Value)
+                {
+                    log.Info("Added new entry to Pwned Passwords");
+                }
+                else
+                {
+                    log.Info("Updated existing entry in Pwned Passwords");
+                }
+
+                return PwnedResponse.CreateResponse(req, HttpStatusCode.OK, "");
+            }
+            catch (Newtonsoft.Json.JsonReaderException)
             {
-                // Prevalence not set or invalid value, bad request
+                // Everything can be string, but Prevalence must be an int, so it can cause a JsonReader exception
                 return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing or invalid prevalence value");
             }
-
-            log.Info("Received valid Pwned Passwords append request");
-
-            BlobStorage storage = new BlobStorage(log);
-            bool? newEntry = storage.UpdateHash(data.SHA1Hash, data.Prevalence);
-            if (newEntry == null)
-            {
-                // Returned null, that means that the item was unable to be added, internal server error
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.InternalServerError, "Unable to add entry to Pwned Passwords");
-            }
-
-            if (newEntry.Value)
-            {
-                log.Info("Added new entry to Pwned Passwords");
-            }
-            else
-            {
-                log.Info("Updated existing entry in Pwned Passwords");
-            }
-
-            return PwnedResponse.CreateResponse(req, HttpStatusCode.OK, "");
         }
     }
 }
