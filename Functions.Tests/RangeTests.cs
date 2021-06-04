33f0bbbd5ebfd7659138e3a0725389d501f059d4
Vinny Martins <martins.vds@outlook.com>
2021-06-04 13:55:33 -0600

    fix broken tests

--- Functions.Tests/RangeTests.cs
+++ Functions.Tests/RangeTests.cs
@@ -1,15 +1,12 @@
-﻿using FluentAssertions;
-using Functions.Dtos;
-using Functions.Services.HttpResponder;
-using Functions.Services.Storage;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Net.Http.Headers;
 using Moq;
-using System;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Functions.Tests
@@ -22,28 +19,17 @@ namespace Functions.Tests
             var validHashPrefix = "ABCDE";
             var lastModified = "Fri, 01 Jan 2021 00:00:00 GMT";
             var request = new TestHttpRequestData(new TestFunctionContext());
-            var responderService = new DefaultHttpResponderService();
-            var nullLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
-            var returnHashFile = new HashFile()
-            {
-                Content = Array.Empty<byte>(),
-                LastModified = DateTimeOffset.Parse(lastModified)
-            };
+            var dummyLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
+            var returnHashFile = new BlobStorageEntry(Stream.Null, DateTimeOffset.Parse(lastModified));
 
             var mockStorage = new Mock<IStorageService>();
-            mockStorage.Setup(s => s.GetHashesByPrefix(validHashPrefix, CancellationToken.None)).ReturnsAsync(returnHashFile);
+            mockStorage.Setup(s => s.GetByHashesByPrefix(validHashPrefix)).ReturnsAsync(returnHashFile);
 
-            var function = new Range(responderService, mockStorage.Object, nullLogger);
+            var function = new Range(mockStorage.Object, dummyLogger);
 
-            var actualResponse = await function.RunRoute(request, validHashPrefix);
+            var actualResponse = await function.RunAsync(request, validHashPrefix);
 
             actualResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-
-            actualResponse.Headers.GetValues(HeaderNames.ContentType).Should().BeEquivalentTo(new string[] { "text/plain" });
-            actualResponse.Headers.GetValues(HeaderNames.AccessControlAllowOrigin).Should().BeEquivalentTo(new string[] { "*" });
-            actualResponse.Headers.GetValues(HeaderNames.CacheControl).Should().ContainMatch("*public*max-age=*");
-            actualResponse.Headers.GetValues(HeaderNames.LastModified).Should().BeEquivalentTo(new string[] { lastModified });
-            actualResponse.Headers.GetValues(ExtendedHeaderNames.ArrDisableSessionAffinity).Should().BeEquivalentTo(new string[] { "True" });
         }
 
         [Theory]
@@ -55,12 +41,12 @@ namespace Functions.Tests
         public async Task Returns_bad_request_given_invalid_hashprefix(string invalidHashPrefix)
         {
             var request = new TestHttpRequestData(new TestFunctionContext());
-            var responderService = new DefaultHttpResponderService();
-            var nullLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
+            var mockStorage = new Mock<IStorageService>();
+            var dummyLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
 
-            var function = new Range(responderService, null, nullLogger);
+            var function = new Range(mockStorage.Object, dummyLogger);
 
-            var actualResponse = await function.RunRoute(request, invalidHashPrefix);
+            var actualResponse = await function.RunAsync(request, invalidHashPrefix);
 
             actualResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
@@ -69,17 +55,15 @@ namespace Functions.Tests
         public async Task Returns_internal_server_error_when_something_goes_wrong()
         {
             var validHashPrefix = "ABCDE";
-
             var request = new TestHttpRequestData(new TestFunctionContext());
-            var responderService = new DefaultHttpResponderService();
-            var nullLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
+            var dummyLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
 
             var mockStorage = new Mock<IStorageService>();
-            mockStorage.Setup(s => s.GetHashesByPrefix(It.IsAny<string>(), CancellationToken.None)).ThrowsAsync(new System.Exception());
+            mockStorage.Setup(s => s.GetByHashesByPrefix(It.IsAny<string>())).ThrowsAsync(new Exception());
 
-            var function = new Range(responderService, mockStorage.Object, nullLogger);
+            var function = new Range(mockStorage.Object, dummyLogger);
 
-            var actualResponse = await function.RunRoute(request, validHashPrefix);
+            var actualResponse = await function.RunAsync(request, validHashPrefix);
 
             actualResponse.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
         }
