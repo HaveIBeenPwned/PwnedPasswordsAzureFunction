d466365695c0e762a108d5e64e23202049823411
Vinny Martins <martins.vds@outlook.com>
2021-06-09 11:36:03 -0600

    add tests and make small refactors

---
+++ Functions.Tests/Helpers/TestBindingMetadata.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace Functions.Tests
+{
+    // Copied from https://github.com/Azure/azure-functions-dotnet-worker/blob/main/test/DotNetWorkerTests/TestBindingMetadata.cs
+    public class TestBindingMetadata : BindingMetadata
+    {
+        public TestBindingMetadata(string type, BindingDirection direction)
+        {
+            Type = type;
+            Direction = direction;
+        }
+
+        public override string Type { get; }
+
+        public override BindingDirection Direction { get; }
+    }
+}
---
+++ Functions.Tests/RangeTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Functions.Tests
+{
+    public class RangeTests
+    {
+        [Fact]
+        public async Task Returns_ok_given_valid_hashprefix()
+        {
+            var validHashPrefix = "ABCDE";
+            var lastModified = "Fri, 01 Jan 2021 00:00:00 GMT";
+            var request = new TestHttpRequestData(new TestFunctionContext());
+            var dummyLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
+            var returnHashFile = new BlobStorageEntry(Stream.Null, DateTimeOffset.Parse(lastModified));
+
+            var mockStorage = new Mock<IStorageService>();
+            mockStorage.Setup(s => s.GetHashesByPrefix(validHashPrefix)).ReturnsAsync(returnHashFile);
+
+            var function = new Range(mockStorage.Object, dummyLogger);
+
+            var actualResponse = await function.RunAsync(request, validHashPrefix);
+
+            actualResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("123456")]
+        [InlineData("ABCDG")]
+        [InlineData("ghijk")]
+        public async Task Returns_bad_request_given_invalid_hashprefix(string invalidHashPrefix)
+        {
+            var request = new TestHttpRequestData(new TestFunctionContext());
+            var mockStorage = new Mock<IStorageService>();
+            var dummyLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
+
+            var function = new Range(mockStorage.Object, dummyLogger);
+
+            var actualResponse = await function.RunAsync(request, invalidHashPrefix);
+
+            actualResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Returns_internal_server_error_when_something_goes_wrong()
+        {
+            var validHashPrefix = "ABCDE";
+            var request = new TestHttpRequestData(new TestFunctionContext());
+            var dummyLogger = NullLoggerFactory.Instance.CreateLogger<Range>();
+
+            var mockStorage = new Mock<IStorageService>();
+            mockStorage.Setup(s => s.GetHashesByPrefix(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+            var function = new Range(mockStorage.Object, dummyLogger);
+
+            var actualResponse = await function.RunAsync(request, validHashPrefix);
+
+            actualResponse.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+    }
+}
