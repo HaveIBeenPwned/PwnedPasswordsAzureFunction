bd318fb31751ece535ffd0583697fd849d5f3923
David Pine <david.pine@microsoft.com>
2021-08-06 11:12:49 -0500

    remove copyright that was automatically applied from my local settings

--- Functions/HttpRequestDataExtensions.cs
+++ Functions/HttpRequestDataExtensions.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Microsoft.Azure.Functions.Worker.Http;
+﻿using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Functions
